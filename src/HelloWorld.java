61901f7542cdf2c5fd92031c5a312c36e02b4b89
truyenho95 <truyenho.webdev@gmail.com>
2018-11-29 06:02:19 +0700

    Add HelloWorld.java .gitignore

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+  // psvm + tab: viet nhanh ham m
+  public static void main(String[] args) {
+    // sout + tab: viet nhanh ham System.out.println
+    System.out.println("Xin chào thế giới nhé! Hehe");
+  }
+}
