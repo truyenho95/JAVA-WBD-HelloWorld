c87816916b1619c59d22aa93d9064e7999d88f13
truyenho95 <truyenho.webdev@gmail.com>
2018-11-29 22:23:14 +0700

    Hoc cach nhap lieu tu ban phim trong Java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class HelloWorld {
   // psvm + tab: viet nhanh ham m
   public static void main(String[] args) {
     // sout + tab: viet nhanh ham System.out.println
     System.out.println("Xin chào thế giới nhé! Hehe");
+    /*
+     * Scanner la kieu du lieu cu object scanner
+     * System.in la lenh lay du lieu nhap tu ban phim
+    */
+    System.out.println("Nhập số nguyên:");
+    Scanner input1 = new Scanner(System.in);
+    int a = input1.nextInt();
+    System.out.println("Bạn vừa nhập: " + a);
+
+    System.out.println("Nhập số double:");
+    double b = new Scanner(System.in).nextDouble();
+    System.out.println("Bạn vừa nhập: " + b);
+
+    System.out.println("Nhập số float:");
+    float c = new Scanner(System.in).nextFloat();
+    System.out.println("Bạn vừa nhập: " + c);
+
+    System.out.println("Mời nhập tên:");
+    String str = new Scanner(System.in).nextLine();
+    System.out.println("Bạn vừa nhập: " + str);
   }
 }
